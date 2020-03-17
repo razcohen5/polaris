079ad908d6f86daf62883737c4d2d56c2fa5761d
Arik F <furmanmail@gmail.com>
2020-03-17 11:00:17 +0200

    fix: fixed reviews

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,6 @@
 import * as loggerConfiguration from "../../resources/logger-configuration.json";
-import {PolarisLogger} from "@enigmatis/polaris-core";
+import {PolarisGraphQLLogger} from "@enigmatis/polaris-core";
 import {applicationProperties} from "../main";
 
-export const logger = new PolarisLogger(loggerConfiguration, applicationProperties);
+export const logger = new PolarisGraphQLLogger(loggerConfiguration, applicationProperties);
 export {loggerConfiguration}
